31f1f4e311a58543fb8c97c7c96d49e0af14328a
Ayeah Godlove A <ayeahgodlove5@gmail.com>
2023-04-12 10:39:42 +0100

    feat(app-layout): design application layout

---
+++ src/components/app-container/app-container.style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { backgroundColor } from "../../utils/colors";
+
+export const AppContainerStyle = styled.div`
+    align-items: flex-start;
+    background-color: ${backgroundColor};
+    display: flex;
+    flex-direction: row;
+    height: 100%;
+    padding: 20px;
+    width: 100%;
+`;
---
+++ src/utils/colors.ts
@@ -0,0 +1,1 @@
+export const backgroundColor= "#3179ba";
